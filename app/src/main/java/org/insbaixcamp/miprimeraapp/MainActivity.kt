520f1524a3066b193a43da886bd9d28ff8f2b49e
= <=>
2021-02-08 18:36:23 +0100

    anexos+ git version actividad

--- app/src/main/java/org/insbaixcamp/miprimeraapp/MainActivity.kt
+++ app/src/main/java/org/insbaixcamp/miprimeraapp/MainActivity.kt
@@ -4,7 +4,7 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
+import android.widget.EditText  // version con git
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import org.insbaixcamp.miprimeraapp.TasApplication.Companion.prefs
